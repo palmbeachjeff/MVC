cd5c132da0bfad6e37eecc52fe4ab8e23a99abd7
palmbeachjeff <palmbeachjeff@gmail.com>
2015-02-08 21:39:41 -0500

    added first model

--- MVC/db/schema.rb
+++ MVC/db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209014658) do
+ActiveRecord::Schema.define(version: 20150209022148) do
 
   create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
